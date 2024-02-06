01e62bca4d9cd9156bd81900a97c0e45586de881
Rokotilli <108419236+Rokotilli@users.noreply.github.com>
2024-02-06 22:26:11 +0200

    Delete Fake images directory
    deleted unnecessary folder

--- Fake images/Models/Additional/FakeImageRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fake_images.Models.Additional
-{
-    public class FakeImageRequest
-    {
-        public string Name { get; set; }
-        public IFormFile Photo {  get; set; }
-        public IFormFile BackGround { get; set; }
-    }
-}
